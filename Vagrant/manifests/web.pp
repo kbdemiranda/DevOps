f9706227553c6ad012c3293df887e86303a9fd4d
Kaique de Miranda <kbdemiranda@gmail.com>
2018-07-25 19:38:40 -0300

    Configuração de serviços e instalação da aplicação
    Adicionado a configuração de serviço do tomcat e a copia do arquivo .war para dentro da aplicação, solicitando após a copiar que o serviço seja reiniciado.

--- Vagrant/manifests/web.pp
+++ Vagrant/manifests/web.pp
@@ -5,3 +5,20 @@ package { ["openjdk-7-jre", "tomcat7"]:
     ensure => installed,
     require => Exec["apt-update"]
 }
+
+service { "tomcat7":
+    ensure     => running, # Verifica se o serviço esta em execução.
+    enable     => true, #  Verifica se esta ligado.
+    hasrestart => true, # Verifica se o serviço de restart esta disponivel.
+    hasstatus  => true, # Verifica se o serviço de status esta disponivel.
+    require => Package["tomcat7"] # Verifica se o pacote de tomcat7 esta instalado.
+}
+
+file { '/var/lib/tomcat7/webapp/vrapvraptor-musicjungle.war':
+    source => '/vagrant/manifests/vrapvraptor-musicjungle.war';
+    owner  => tomcat7,
+    group  => tomcat7,
+    mode   => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
